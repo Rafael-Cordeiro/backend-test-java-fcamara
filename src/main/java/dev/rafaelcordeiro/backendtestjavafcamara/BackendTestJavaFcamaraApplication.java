a98374758627abb38be3d54d9a47d0a0b79ef767
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-09-09 01:43:11 -0300

    [codebase] feat: Cria estrutura do projeto

---
+++ src/main/java/dev/rafaelcordeiro/backendtestjavafcamara/BackendTestJavaFcamaraApplication.java
@@ -0,0 +1,13 @@
+package dev.rafaelcordeiro.backendtestjavafcamara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTestJavaFcamaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTestJavaFcamaraApplication.class, args);
+    }
+
+}
