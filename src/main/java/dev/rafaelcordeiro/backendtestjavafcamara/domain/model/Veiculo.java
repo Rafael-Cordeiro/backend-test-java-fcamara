90afef35364f648d80b707edcead34cf68eda186
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-09-10 02:35:24 -0300

    [domain] feat: Entidade Veículo

---
+++ src/main/java/dev/rafaelcordeiro/backendtestjavafcamara/domain/model/Veiculo.java
@@ -0,0 +1,21 @@
+package dev.rafaelcordeiro.backendtestjavafcamara.domain.model;
+
+import dev.rafaelcordeiro.backendtestjavafcamara.domain.DomainEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity(name = "veiculo")
+@Data
+public class Veiculo extends DomainEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String marca;
+    private String modelo;
+    private String cor;
+    private String placa;
+    private String tipo;
+}
