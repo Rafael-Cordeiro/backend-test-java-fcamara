970b5bd02dc97c58d4d2a85eb9a8fa2f77648117
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-09-10 00:16:27 -0300

    [codebase] chore: Adiciona script sql para inicializar dados padrão no banco H2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO estabelecimento (nome, cnpj, endereco, telefone, qtde_vagas_motos, qtde_vagas_carros)
+VALUES ('Estapar', '12.345.678/0001-90', 'Rua cinco, 454 - São Paulo - SP', '(11) 98765-4321', 25, 15);
