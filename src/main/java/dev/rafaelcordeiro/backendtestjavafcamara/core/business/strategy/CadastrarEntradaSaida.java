e9038782be5fbf5e5f0e1a249f4153cf639d070b
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-09-10 15:24:20 -0300

    [core] feat: Strategy que cadastra entrada ou saída

---
+++ src/main/java/dev/rafaelcordeiro/backendtestjavafcamara/core/business/strategy/CadastrarEntradaSaida.java
@@ -0,0 +1,31 @@
+package dev.rafaelcordeiro.backendtestjavafcamara.core.business.strategy;
+
+import dev.rafaelcordeiro.backendtestjavafcamara.core.business.BusinessCase;
+import dev.rafaelcordeiro.backendtestjavafcamara.domain.model.ControleEntradaSaida;
+import dev.rafaelcordeiro.backendtestjavafcamara.infra.db.ControleEntradaSaidaRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@Slf4j
+public class CadastrarEntradaSaida implements IStrategy<ControleEntradaSaida> {
+
+    @Autowired
+    ControleEntradaSaidaRepository repository;
+
+    @Override
+    public void execute(ControleEntradaSaida entity, BusinessCase<ControleEntradaSaida> businessCase) {
+        try {
+            if (businessCase.getResult().getSuccess()) {
+                businessCase.getResult().setEntity(Optional.of(repository.save(entity)));
+            } else {
+                log.error("Status de erro na execução das validações. Abortando cadastro de entrada ou saída");
+            }
+        } catch (Exception e) {
+            handleException(log, e, businessCase);
+        }
+    }
+}
