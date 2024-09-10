68ae5a2c8a61e6ac8733dd6571270a0771600580
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-09-10 02:50:13 -0300

    [codebase] feat: adiciona mais dados de estabelecimentos e novos dados de veículos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,9 @@
-INSERT INTO estabelecimento (nome, cnpj, endereco, telefone, qtde_vagas_motos, qtde_vagas_carros)
-VALUES ('Estapar', '12.345.678/0001-90', 'Rua cinco, 454 - São Paulo - SP', '(11) 98765-4321', 25, 15);
+INSERT INTO estabelecimento (nome, cnpj, endereco, telefone, qtde_vagas_motos, qtde_vagas_carros) VALUES
+('Estapar', '12.345.678/0001-90', 'Rua cinco, 454 - São Paulo - SP', '(11) 98765-4321', 25, 15),
+('Helbor Parking', '63.103.442/0001-83', 'Rua Dr Ricardo Vilela, 300 - Mogi das Cruzes - SP', '(11) 98765-4321', 25, 15);
+
+INSERT INTO veiculo (marca, modelo, cor, placa, tipo) VALUES
+('Fiat', 'Uno Mille 2002', 'Vermelho', 'FEB-8879', 'B'),
+('Chevrolet', 'Onix Hatch 2018', 'Branco', 'CEI-3Y22', 'B'),
+('Yamaha', 'Fazer FZ25 ABS', 'Azul', 'SPO-4L21', 'A'),
+('BMW', 'G 310 GS', 'Preto', 'GZW-7546', 'A');
