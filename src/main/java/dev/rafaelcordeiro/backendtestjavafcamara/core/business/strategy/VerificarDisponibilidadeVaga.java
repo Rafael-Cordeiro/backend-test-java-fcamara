1d0123e77d187b7f1ebd5a24d68693fa2468255b
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-09-10 17:57:38 -0300

    [core] feat: Verificação de vagas no estacionamento antes de cadastrar CES

---
+++ src/main/java/dev/rafaelcordeiro/backendtestjavafcamara/core/business/strategy/VerificarDisponibilidadeVaga.java
@@ -0,0 +1,48 @@
+package dev.rafaelcordeiro.backendtestjavafcamara.core.business.strategy;
+
+import dev.rafaelcordeiro.backendtestjavafcamara.core.business.BusinessCase;
+import dev.rafaelcordeiro.backendtestjavafcamara.domain.model.ControleEntradaSaida;
+import dev.rafaelcordeiro.backendtestjavafcamara.domain.model.EntradaSaidaEnum;
+import dev.rafaelcordeiro.backendtestjavafcamara.infra.db.ControleEntradaSaidaRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@Slf4j
+public class VerificarDisponibilidadeVaga implements IStrategy<ControleEntradaSaida> {
+
+    @Autowired
+    ControleEntradaSaidaRepository repository;
+
+    @Override
+    public void execute(ControleEntradaSaida entity, BusinessCase<ControleEntradaSaida> businessCase) {
+        try {
+            if (businessCase.getResult().getSuccess() && entity.getEntradaSaida().equals(EntradaSaidaEnum.ENTRADA)) {
+                var entradasSaidas = repository.findByEstabelecimento(entity.getEstabelecimento())
+                        .stream().filter(it -> it.getVeiculo().getTipo().equals(entity.getVeiculo().getTipo())).toList();
+
+                var vagasOcupadas = entradasSaidas.stream().filter(it -> it.getEntradaSaida().equals(EntradaSaidaEnum.ENTRADA)).count() -
+                        entradasSaidas.stream().filter(it -> it.getEntradaSaida().equals(EntradaSaidaEnum.SAIDA)).count();
+
+                if (entity.getVeiculo().getTipo().equals("A") && vagasOcupadas >= entity.getEstabelecimento().getQtdeVagasMotos()) {
+                    businessCase.getResult().setSuccess(false);
+                    businessCase.getResult().getMessages().add("O limite para vagas de moto do estabelecimento (ID " +
+                            entity.getEstabelecimento().getId() + ") foi atingido. (Limite: " +
+                            entity.getEstabelecimento().getQtdeVagasMotos() + ")");
+                }
+                if (entity.getVeiculo().getTipo().equals("B") && vagasOcupadas >= entity.getEstabelecimento().getQtdeVagasCarros()) {
+                    businessCase.getResult().setSuccess(false);
+                    businessCase.getResult().getMessages().add("O limite para vagas de carro do estabelecimento (ID " +
+                            entity.getEstabelecimento().getId() + ") foi atingido. (Limite: " +
+                            entity.getEstabelecimento().getQtdeVagasCarros() + ")");
+                }
+            }
+        } catch (Exception e) {
+            handleException(log, e, businessCase);
+        }
+    }
+}
