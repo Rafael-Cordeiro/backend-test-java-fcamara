95d3ef2a2f1f9996376f61140022dc718bc21a97
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-09-10 02:38:53 -0300

    [core] feat: Strategy deleta veículo

---
+++ src/main/java/dev/rafaelcordeiro/backendtestjavafcamara/core/business/strategy/DeleteVeiculo.java
@@ -0,0 +1,36 @@
+package dev.rafaelcordeiro.backendtestjavafcamara.core.business.strategy;
+
+import dev.rafaelcordeiro.backendtestjavafcamara.core.business.BusinessCase;
+import dev.rafaelcordeiro.backendtestjavafcamara.domain.model.Veiculo;
+import dev.rafaelcordeiro.backendtestjavafcamara.infra.db.VeiculoJPARepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class DeleteVeiculo implements IStrategy<Veiculo> {
+
+    @Autowired
+    VeiculoJPARepository repository;
+
+    @Override
+    public void execute(Veiculo entity, BusinessCase<Veiculo> businessCase) {
+        try {
+            if (businessCase.getResult().getSuccess()) {
+                if (entity.getId() != null) {
+                    var persistedEntity = repository.findById(entity.getId()).orElse(null);
+                    if (persistedEntity != null) {
+                        repository.delete(persistedEntity);
+                    } else throw new Exception("A entidade analisada possui ID (" + entity.getId() + "), mas nenhum registro foi encontrado no banco de dados");
+                }
+                else throw new Exception("A entidade analisada não possui ID");
+            } else {
+                log.error("Status de erro na execução das validações. Abortando exclusão de veículo");
+            }
+        } catch (Exception e) {
+            handleException(log, e, businessCase);
+        }
+    }
+}
+
