085cbbc9b14d23381c13a11bcd3f3108a1940ea0
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-09-10 15:25:31 -0300

    [core] feat: Validação de entrada em aberto para critério de cadastro de entrada ou saída

---
+++ src/main/java/dev/rafaelcordeiro/backendtestjavafcamara/core/business/strategy/ValidarEntradaSaida.java
@@ -0,0 +1,51 @@
+package dev.rafaelcordeiro.backendtestjavafcamara.core.business.strategy;
+
+import dev.rafaelcordeiro.backendtestjavafcamara.core.business.BusinessCase;
+import dev.rafaelcordeiro.backendtestjavafcamara.domain.model.ControleEntradaSaida;
+import dev.rafaelcordeiro.backendtestjavafcamara.domain.model.EntradaSaidaEnum;
+import dev.rafaelcordeiro.backendtestjavafcamara.infra.db.ControleEntradaSaidaRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+@Component
+@Slf4j
+public class ValidarEntradaSaida implements IStrategy<ControleEntradaSaida> {
+
+    @Autowired
+    ControleEntradaSaidaRepository repository;
+
+    @Override
+    public void execute(ControleEntradaSaida entity, BusinessCase<ControleEntradaSaida> businessCase) {
+        try {
+            List<ControleEntradaSaida> entradasSaidas = repository.findByVeiculoAndEstabelecimento(entity.getVeiculo(), entity.getEstabelecimento());
+            switch (entity.getEntradaSaida()) {
+                case EntradaSaidaEnum.ENTRADA:
+                    if (Collections.max(entradasSaidas, Comparator.comparing(ControleEntradaSaida::getRegistro))
+                            .getEntradaSaida().equals(EntradaSaidaEnum.ENTRADA)) {
+                        businessCase.getResult().setSuccess(false);
+                        businessCase.getResult().getMessages()
+                                .add("Não pode ser cadastrada uma entrada do veículo de ID (" +
+                                        entity.getVeiculo().getId() + ") no estabelecimento de ID (" +
+                                        entity.getEstabelecimento().getId() + "), pois ainda consta uma entrada em aberto");
+                    }
+                    break;
+                case EntradaSaidaEnum.SAIDA:
+                    if (Collections.max(entradasSaidas, Comparator.comparing(ControleEntradaSaida::getRegistro))
+                            .getEntradaSaida().equals(EntradaSaidaEnum.SAIDA)) {
+                        businessCase.getResult().setSuccess(false);
+                        businessCase.getResult().getMessages().add("Não pode ser cadastrada uma saída do veículo de ID (" +
+                                entity.getVeiculo().getId() + ") no estabelecimento de ID (" +
+                                entity.getEstabelecimento().getId() + "), pois não consta nenhuma entrada em aberto");
+                    }
+                    break;
+            }
+        } catch (Exception e) {
+            handleException(log, e, businessCase);
+        }
+    }
+}
