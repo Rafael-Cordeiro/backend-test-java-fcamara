14abebd24930d8237f80267720406f2ecf2e6d81
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-09-10 01:15:02 -0300

    [api] refactor: EstabelecimentoController respondendo com APIResponse e tratando exceção de strategy

--- src/main/java/dev/rafaelcordeiro/backendtestjavafcamara/api/EstabelecimentoController.java
+++ src/main/java/dev/rafaelcordeiro/backendtestjavafcamara/api/EstabelecimentoController.java
@@ -1,6 +1,7 @@
 package dev.rafaelcordeiro.backendtestjavafcamara.api;
 
 import dev.rafaelcordeiro.backendtestjavafcamara.core.facade.CRUDFacade;
+import dev.rafaelcordeiro.backendtestjavafcamara.core.util.InvalidStrategyConditionException;
 import dev.rafaelcordeiro.backendtestjavafcamara.domain.model.Estabelecimento;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/estabelecimento")
@@ -23,27 +24,62 @@ public class EstabelecimentoController {
     private CRUDFacade<Estabelecimento> facade;
 
     @PostMapping
-    public ResponseEntity<Estabelecimento> createEstabelecimento(@RequestBody Estabelecimento estabelecimento) {
-        return ResponseEntity.ok(facade.persist(estabelecimento, "PERSISTE_ESTABELECIMENTO"));
+    public ResponseEntity<APIResponse<Estabelecimento>> createEstabelecimento(@RequestBody Estabelecimento estabelecimento) {
+        var result = new APIResponse<Estabelecimento>();
+        try {
+            result.setEntity(facade.persist(estabelecimento, "PERSISTE_ESTABELECIMENTO"));
+            result.setSuccess(true);
+            return ResponseEntity.ok(result);
+        } catch (InvalidStrategyConditionException e) {
+            result.setMessages(Arrays.stream(e.getMessage().split("\n")).toList());
+            result.setSuccess(false);
+            return ResponseEntity.unprocessableEntity().body(result);
+        }
     }
 
     @GetMapping
-    public ResponseEntity<List<Estabelecimento>> getEstabelecimento() {
-        return ResponseEntity.ok(facade.findAll(new Estabelecimento(), "FINDALL_ESTABELECIMENTOS"));
+    public ResponseEntity<APIResponse<Estabelecimento>> getEstabelecimento() {
+        var result = new APIResponse<Estabelecimento>();
+        try {
+            result.setEntities(facade.findAll(new Estabelecimento(), "FINDALL_ESTABELECIMENTOS"));
+            result.setSuccess(true);
+            return ResponseEntity.ok(result);
+        } catch (InvalidStrategyConditionException e) {
+            result.setMessages(Arrays.stream(e.getMessage().split("\n")).toList());
+            result.setSuccess(false);
+            return ResponseEntity.badRequest().body(result);
+        }
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Estabelecimento> updateEstabelecimento(@RequestBody Estabelecimento estabelecimento, @PathVariable Long id) {
+    public ResponseEntity<APIResponse<Estabelecimento>> deleteEstabelecimento(@RequestBody Estabelecimento estabelecimento, @PathVariable Long id) {
         estabelecimento.setId(id);
-        return ResponseEntity.ok(facade.persist(estabelecimento, "PERSISTE_ESTABELECIMENTO"));
+        var result = new APIResponse<Estabelecimento>();
+        try {
+            result.setEntity(facade.persist(estabelecimento, "PERSISTE_ESTABELECIMENTO"));
+            result.setSuccess(true);
+            return ResponseEntity.ok(result);
+        } catch (InvalidStrategyConditionException e) {
+            result.setMessages(Arrays.stream(e.getMessage().split("\n")).toList());
+            result.setSuccess(false);
+            return ResponseEntity.unprocessableEntity().body(result);
+        }
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<String> updateEstabelecimento(@PathVariable Long id) {
+    public ResponseEntity<APIResponse<Estabelecimento>> deleteEstabelecimento(@PathVariable Long id) {
         var estabelecimento = new Estabelecimento();
         estabelecimento.setId(id);
-        facade.delete(estabelecimento, "DELETE_ESTABELECIMENTO");
-        return ResponseEntity.ok("Deletado");
+        var result = new APIResponse<Estabelecimento>();
+        try {
+            facade.delete(estabelecimento, "DELETE_ESTABELECIMENTO");
+            result.setSuccess(true);
+            return ResponseEntity.ok(result);
+        } catch (InvalidStrategyConditionException e) {
+            result.setMessages(Arrays.stream(e.getMessage().split("\n")).toList());
+            result.setSuccess(false);
+            return ResponseEntity.status(404).body(result);
+        }
     }
 
 
