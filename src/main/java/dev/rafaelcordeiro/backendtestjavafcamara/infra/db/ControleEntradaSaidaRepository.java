b44a9f9e6facbb6dbc4b963107390d0c0afe59f3
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-09-10 15:23:42 -0300

    [infra] feat: Criação de repository para controle de entrada e saída

---
+++ src/main/java/dev/rafaelcordeiro/backendtestjavafcamara/infra/db/ControleEntradaSaidaRepository.java
@@ -0,0 +1,14 @@
+package dev.rafaelcordeiro.backendtestjavafcamara.infra.db;
+
+import dev.rafaelcordeiro.backendtestjavafcamara.domain.model.ControleEntradaSaida;
+import dev.rafaelcordeiro.backendtestjavafcamara.domain.model.Estabelecimento;
+import dev.rafaelcordeiro.backendtestjavafcamara.domain.model.Veiculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ControleEntradaSaidaRepository extends JpaRepository<ControleEntradaSaida, Long> {
+    List<ControleEntradaSaida> findByVeiculoAndEstabelecimento(Veiculo veiculo, Estabelecimento estabelecimento);
+}
