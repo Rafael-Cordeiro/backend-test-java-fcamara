48f4f25e03f69b82caa94e48b7a28f935446e3a8
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-09-10 00:27:15 -0300

    [core] feat: Exception para sinalizar erros de strategy

---
+++ src/main/java/dev/rafaelcordeiro/backendtestjavafcamara/core/util/InvalidStrategyConditionException.java
@@ -0,0 +1,23 @@
+package dev.rafaelcordeiro.backendtestjavafcamara.core.util;
+
+public class InvalidStrategyConditionException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidStrategyConditionException () {
+        super();
+    }
+
+    public InvalidStrategyConditionException ( String message , Throwable cause ) {
+        super( message , cause );
+    }
+
+    public InvalidStrategyConditionException ( String message ) {
+        super( message );
+    }
+
+    public InvalidStrategyConditionException ( Throwable cause ) {
+        super( cause );
+    }
+
+}
