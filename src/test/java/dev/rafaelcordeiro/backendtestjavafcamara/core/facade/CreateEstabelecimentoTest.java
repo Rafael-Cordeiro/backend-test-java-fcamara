2935edd0e574c077c3dda377c8c2735a0e2a712a
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-09-10 00:29:36 -0300

    [core] test: Adaptação de teste de cadastro de estabelecimento para o uso de facade como bean (WIP)

--- src/test/java/dev/rafaelcordeiro/backendtestjavafcamara/core/facade/CreateEstabelecimentoTest.java
+++ src/test/java/dev/rafaelcordeiro/backendtestjavafcamara/core/facade/CreateEstabelecimentoTest.java
@@ -2,16 +2,43 @@ package dev.rafaelcordeiro.backendtestjavafcamara.core.facade;
 
 import dev.rafaelcordeiro.backendtestjavafcamara.domain.model.Estabelecimento;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = {CRUDFacade.class})
+//@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 public class CreateEstabelecimentoTest {
+
+    @Autowired
+    @InjectMocks
+    private CRUDFacade<Estabelecimento> facade;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("Valida se bean facade não é nulo")
+    public void assert_that_facade_not_null() {
+        assertNotNull(facade);
+    }
 
     @Test
     public void should_be_able_to_create_estabelecimento() {
         // criar novo estab
         // criar tabela estab (Entidade)
         Estabelecimento estabelecimento = new Estabelecimento(
+                1l,
                 "Estapar",
                 "12.345.678/0001-90",
                 "Rua cinco - São Paulo",
@@ -20,9 +47,8 @@ public class CreateEstabelecimentoTest {
                 15
         );
 
-        // criar service
-        CRUDFacade<Estabelecimento> facade = new CRUDFacade<>();
-        Estabelecimento createdEstabelecimento = facade.create(estabelecimento);
+//        var fachada = new CRUDFacade<Estabelecimento>();
+        Estabelecimento createdEstabelecimento = facade.persist(estabelecimento, "PERSISTE_ESTABELECIMENTO");
 
         assertNotNull(createdEstabelecimento);
         // criar repository estab
