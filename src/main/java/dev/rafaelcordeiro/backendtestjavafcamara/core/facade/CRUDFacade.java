9d3c30ae8d575b5aba9f240571bd37e97542d2aa
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-09-10 00:25:38 -0300

    [core] feat: Adaptação de fachada para compor métodos de CRUD utilizando o subsistema de execução de strategies

--- src/main/java/dev/rafaelcordeiro/backendtestjavafcamara/core/facade/CRUDFacade.java
+++ src/main/java/dev/rafaelcordeiro/backendtestjavafcamara/core/facade/CRUDFacade.java
@@ -1,32 +1,63 @@
 package dev.rafaelcordeiro.backendtestjavafcamara.core.facade;
 
+import dev.rafaelcordeiro.backendtestjavafcamara.core.business.BusinessCase;
+import dev.rafaelcordeiro.backendtestjavafcamara.core.business.BusinessCasesExecutor;
+import dev.rafaelcordeiro.backendtestjavafcamara.core.util.InvalidStrategyConditionException;
 import dev.rafaelcordeiro.backendtestjavafcamara.domain.DomainEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class CRUDFacade<E extends DomainEntity> implements ICRUDFacade<E> {
+
+    @Autowired
+    BusinessCasesExecutor executor;
+
     @Override
-    public E create(E entity) {
-        return null;
+    public E persist(E entity, String caseName) {
+        BusinessCase<E> businessCase = new BusinessCase<E>().builder()
+                .withName(caseName)
+                .build();
+        executor.run(entity, businessCase);
+
+        if (!businessCase.getResult().getSuccess())
+            throw new InvalidStrategyConditionException(businessCase.getResult().getPlainMessages());
+
+        Optional<E> optionalPersistedEntity = businessCase.getResult().getEntity();
+        return optionalPersistedEntity.orElse(null);
     }
 
     @Override
-    public E update(E entity) {
-        return null;
+    public void delete(E entity, String caseName) {
+        BusinessCase<E> businessCase = new BusinessCase<E>().builder()
+                .withName(caseName)
+                .build();
+        executor.run(entity, businessCase);
+
+        if (!businessCase.getResult().getSuccess())
+            throw new InvalidStrategyConditionException(businessCase.getResult().getPlainMessages());
+
     }
 
     @Override
-    public E delete(E entity) {
-        return null;
+    public List<E> findAll(E entity, String caseName) {
+        BusinessCase<E> businessCase = new BusinessCase<E>().builder()
+                .withName(caseName)
+                .build();
+        executor.run(entity, businessCase);
+
+        if (!businessCase.getResult().getSuccess())
+            throw new InvalidStrategyConditionException(businessCase.getResult().getPlainMessages());
+
+        Optional<List<E>> optionalPersistedEntity = businessCase.getResult().getEntities();
+        return optionalPersistedEntity.orElse(null);
     }
 
     @Override
-    public List<E> findAll() {
-        return List.of();
-    }
-
-    @Override
-    public E findById(Long id) {
+    public E findById(Long id, String caseName) {
         return null;
     }
 }
--- src/main/java/dev/rafaelcordeiro/backendtestjavafcamara/core/facade/ICRUDFacade.java
+++ src/main/java/dev/rafaelcordeiro/backendtestjavafcamara/core/facade/ICRUDFacade.java
@@ -5,9 +5,8 @@ import dev.rafaelcordeiro.backendtestjavafcamara.domain.DomainEntity;
 import java.util.List;
 
 public interface ICRUDFacade<E extends DomainEntity> {
-    public E create(E entity);
-    public E update(E entity);
-    public E delete(E entity);
-    public List<E> findAll();
-    public E findById(Long id);
+    public E persist(E entity, String caseName);
+    public void delete(E entity, String caseName);
+    public List<E> findAll(E entity, String caseName);
+    public E findById(Long id, String caseName);
 }
